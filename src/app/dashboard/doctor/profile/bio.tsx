2d044df1ba2e4061b2a39e68ac0aae057bbdcb5f
Khalid <alamutukhalid@gmail.com>
2024-11-25 14:56:15 +0100

    feat:profile-page

---
+++ src/app/dashboard/doctor/profile/bio.tsx
@@ -0,0 +1,21 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { AlertCircle } from "lucide-react";
+
+const Bio = () => {
+	return (
+		<Card className="bg-hubGrey divide-y-2">
+			<CardHeader className="pb-2 flex flex-row gap-x-1">
+				<AlertCircle className="fill-hubPurple text-hubGrey" /> Bio
+			</CardHeader>
+			<CardContent className="pt-2">
+				Dr. Dolor Manchi is a dedicated pediatrician with over 15 years
+				of experience in providing comprehensive healthcare to children.
+				He is passionate about preventive care and patient education,
+				ensuring that families are well-informed about their child's
+				health and wellness.
+			</CardContent>
+		</Card>
+	);
+};
+
+export default Bio;
